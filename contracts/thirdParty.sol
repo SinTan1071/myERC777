bc50e390c28c91f0e50f25dc2b0d0e0b3524cae1
RT7oney <297085213@qq.com>
2020-03-27 14:07:29 +0800

    完成mytoken编写和tokenReceiver编写

--- contracts/thirdParty.sol
+++ contracts/thirdParty.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC777/ERC777.sol";
 
+
+/** 
+    （已弃用）因为实际上ERC777token只能发送给tokenReceiver这类型的智能合约，不能发送给其他类型的智能合约
+    这个第三方的合约类似于一个持币的合约账户，token应该存在于这个address(this)合约中，持有token后拿着这些token去调用send方法，对于send方法所在的erc777合约而言，send方法调用的msg.sender就是这个持币合约
+ */
 contract ThirdParty is Initializable {
     address public tokenAddr;
 
--- contracts/tokenReceiver.sol
+++ contracts/tokenReceiver.sol
@@ -1,21 +1,31 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC777/IERC777.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
+// import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC777/IERC777.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC777/IERC777Recipient.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/introspection/IERC1820Registry.sol";
 
-contract Simple777Recipient is IERC777Recipient {
+import "./interface/ImyToken.sol";
 
+contract TokenReceiver is IERC777Recipient, Initializable, Ownable {
     IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
     bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
 
-    IERC777 private _token;
+    // IERC777 private _token;
+    ImyToken private _token;
 
     event DoneStuff(address operator, address from, address to, uint256 amount, bytes userData, bytes operatorData);
 
-    constructor (address token) public {
-        _token = IERC777(token);
-
+    function initialize(
+        address token
+    )
+    public
+    initializer
+    {
+        Ownable.initialize(_msgSender());
+        // _token = IERC777(token);
+        _token = ImyToken(token);
         _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
     }
 
@@ -26,10 +36,20 @@ contract Simple777Recipient is IERC777Recipient {
         uint256 amount,
         bytes calldata userData,
         bytes calldata operatorData
-    ) external {
-        require(msg.sender == address(_token), "Simple777Recipient: Invalid token");
-
+    ) 
+    external 
+    {
+        require(_msgSender() == address(_token), "Simple777Recipient: Invalid token");
         // do stuff
         emit DoneStuff(operator, from, to, amount, userData, operatorData);
     }
+
+    function receiverMintAndSend(address recipient, uint256 amount, bytes calldata data) 
+    public
+    onlyOwner
+    {
+        _token.mint(address(this), amount);
+        _token.send(recipient, amount, data);
+    }
+
 }
